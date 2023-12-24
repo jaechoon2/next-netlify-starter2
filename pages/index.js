3385742f545e43317a118e258ec8625abf35fa96
Jaechoon Cho <jccho00@gmail.com>
2023-12-24 15:53:18 +0900

    Update index.js start message.

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to Micro Smart AI Hme Page." />
+        <Header title="Welcome to Micro Smart AI Home Page." />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
